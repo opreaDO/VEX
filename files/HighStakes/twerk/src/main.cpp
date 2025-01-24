17981da568f5b7ea596e6e1ed641f86dd1a95aab
Trishan Chudasma <trishan759@gmail.com>
2025-01-24 22:09:31 +0000

    i just bought a property in egypt

--- files/HighStakes/twerk/src/main.cpp
+++ files/HighStakes/twerk/src/main.cpp
@@ -9,12 +9,16 @@ void disabled() {}
 
 void competition_initialize() {}
 
-
 void autonomous() {}
 
 void opcontrol() {
 	LEDmanager.rainbow();
 	while (true) {
+		chassis.opcontrol_tank();
+		poomaticControl();
+		intakeControl();
+		ladyBrownControl();
+		colourSorting();
 		pros::delay(10);
 	}
 }
